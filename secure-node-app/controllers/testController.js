93bf9e4d8dbed734fe47898152bcafac5f6c8166
AKULA-RAMASAI-LAKSHMI-NARASIMHA-TEJA <97424625+AKULA-RAMASAI-LAKSHMI-NARASIMHA-TEJA@users.noreply.github.com>
2025-05-17 12:39:28 +0530

    Delete secure-node-app directory

--- secure-node-app/controllers/testController.js
+++
@@ -1,8 +0,0 @@
-exports.testGet = (req, res) => {
-  res.send("GET request successful!");
-};
-
-exports.testPost = (req, res) => {
-  const { input } = req.body;
-  res.send({ sanitizedInput: input });
-};
